a0e3c1904bc6afb552b605b95bfbce421c5ee58f
Sellan <Sellan@192.168.0.7>
2021-07-09 01:02:30 -0500

    Pequeño update

--- Practicas/src/principal/Principal.java
+++ Practicas/src/principal/Principal.java
@@ -2,6 +2,11 @@ package principal;
 
 public class Principal {
 	public static void main(String[] args ) {
-		
+		 String str_Sample = "a1";
+	        System.out.println("Compare To 'a' b is : " + str_Sample.compareTo("a2"));
+	        str_Sample = "b";
+	        System.out.println("Compare To 'b' a is : " + str_Sample.compareTo("a"));
+	        str_Sample = "b";
+	        System.out.println("Compare To 'b' b is : " + str_Sample.compareTo("b"));
 	}
 }
